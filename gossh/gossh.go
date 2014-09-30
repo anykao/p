67f95983b2c6cdae710bd436ef3c3bbb32d85386
Your Name <you@example.com>
2014-09-30 09:59:00 +0900

    update gh

--- gossh/gossh.go
+++ gossh/gossh.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"time"
 	"flag"
+	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+	"time"
 
 	"code.google.com/p/go.crypto/ssh"
 	"github.com/BurntSushi/toml"
 )
 
 var config string
+
+type logInfo struct {
+	timestamp time.Time
+	host      string
+}
 
 type server struct {
 	IP       string
@@ -26,32 +32,46 @@ type tomlConfig struct {
 	Password string
 }
 
-func init(){
+//var logChan = make(chan, logInfo)
+
+func init() {
 	flag.StringVar(&config, "conf", "", "config file.")
 }
 
 func executeCmd(cmd, hostname string, config *ssh.ClientConfig) string {
-	conn, _ := ssh.Dial("tcp", hostname+":22", config)
-	session, _ := conn.NewSession()
+	conn, err := ssh.Dial("tcp", hostname+":22", config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Println(time.Now(), hostname, "start new session")
+	session, err := conn.NewSession()
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer session.Close()
 
 	output, err := session.CombinedOutput(cmd)
 
 	if err != nil {
-		panic(err)
+		log.Print("ERR: ",err)
 	}
 
 	return "\n\n<<" + hostname + ">>\n" + string(output)
 }
 
 func main() {
+	//go func() {
+		//log.Println(http.ListenAndServe(":6060", nil))
+	//}()
+	log.SetFlags(log.LstdFlags | log.Llongfile)
+
 	flag.Parse()
 	var conf tomlConfig
 	var command string
 	workDir, _ := os.Getwd()
 
 	if config == "" {
-		config =filepath.Join(workDir, "config.toml")
+		config = filepath.Join(workDir, "config.toml")
 	}
 
 	if flag.NArg() > 0 {
@@ -59,11 +79,11 @@ func main() {
 	}
 
 	if _, err := toml.DecodeFile(config, &conf); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	servs := conf.Servers
 	var count int = len(servs)
-	results := make(chan string, 2)
+	results := make(chan string)
 	timeout := time.After(30 * time.Second)
 
 	for _, serv := range servs {
