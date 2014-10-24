94d16173976ef6d3d103a416715c112644298bba
Your Name <you@example.com>
2014-10-24 14:07:26 +0900

    add client

---
+++ bopen/cli/bopen.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+	"github.com/apcera/nats"
+	"github.com/skratchdot/open-golang/open"
+	//"github.com/rakyll/coop"
+)
+
+const(
+	TOPIC = "url"
+	QUEUE = "worker_group"
+)
+
+func main() {
+	f,err := os.Create("opener.log")
+	if err != nil{
+		log.Panic(err)
+	}
+	logger := log.New(f, "logger: ", log.Lshortfile)
+	nc, _ := nats.Connect("nats://172.17.106.112:4222")
+	defer nc.Close()
+	logger.Println("start polling url")
+	nc.QueueSubscribe(TOPIC, QUEUE, func(m *nats.Msg) {
+		open.Start( string(m.Data))
+	})
+	time.Sleep(8 * time.Minute)
+	logger.Println("end polling")
+}
